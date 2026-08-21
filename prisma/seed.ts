b9a87af58707fea0ff397df4273eba05cdfc2c9f
brooke-cloud <shihaoy74@gmail.com>
2026-08-22 02:45:11 +0800

    Fix Prisma import, migrate middleware to proxy, and update dependencies

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from "@prisma/adapter-pg";
 import { problems } from "../data/problems";
 import { posts } from "../data/posts";
