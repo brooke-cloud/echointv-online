8424e89b50cd01dc61bfdd5470c65278a482e966
brooke-cloud <shihaoy74@gmail.com>
2026-08-27 15:31:54 +0800

    add

--- app/(public)/register/page.tsx
+++ app/(public)/register/page.tsx
@@ -206,8 +206,9 @@ export default function RegisterPage() {
           {/* 邮箱验证码 */}
           <div>
             <label className="block text-xs font-semibold uppercase tracking-wider text-gray-300">
-              邮箱验证码
+              邮箱验证码  （如果没收到验证码，请查看垃圾邮件箱）
             </label>
+            
             <div className="relative mt-2">
               <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3.5 text-gray-500">
                 <ShieldCheck className="h-4 w-4" />
@@ -298,7 +299,7 @@ export default function RegisterPage() {
               d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.06l3.66 2.84c.87-2.6 3.3-4.52 6.16-4.52z"
             />
           </svg>
-          <span>使用 Google 账号一键注册 / 登录</span>
+          <span>使用 Google 账号一键注册</span>
         </a>
 
         {/* 底部登录链接 */}
